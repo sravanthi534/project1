fe49bf0ffd6801727f50bf8c0428eb52ef380214
sravanthi <sravanthinagula02@gmail.com>
2024-09-27 16:22:54 +0530

    spring boilerplate code added

---
+++ revshop1/src/main/java/com/Revshop/revshop/RevshopApplication.java
@@ -0,0 +1,13 @@
+package com.Revshop.revshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevshopApplication.class, args);
+	}
+
+}
